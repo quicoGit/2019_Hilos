e4c6e492953f7bc35bd1a47642e1909ff085a244
Quico Orozco <esi@plumbaria.com>
2019-01-08 18:17:37 +0100

    Terminado Ejercicio 5.4. Crear un hilo con AsyncTask

--- app/src/main/java/com/plumbaria/e_5_1_hilos/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_5_1_hilos/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.plumbaria.e_5_1_hilos
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
@@ -10,6 +11,12 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private lateinit var entrada:EditText
     private lateinit var salida:TextView
+
+    /*
+    En kotlin no surgen los mismos problemas que en Java,
+    parece que sabe manejar bien los threads y no
+    interrumpe el programa
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,8 +28,14 @@ class MainActivity : AppCompatActivity() {
     fun calcularOperacion(view:View){
         var n: Int = entrada.text.toString().toInt()
         salida.append("" + n + "! = ")
+        /*
         var thread:Thread = MiThread(n)
         thread.start()
+        */
+        var tarea = MiTarea()
+        tarea.execute(n)
+
+
     }
 
     fun factorial(n: Int): Int {
@@ -47,5 +60,17 @@ class MainActivity : AppCompatActivity() {
             salida.append("" + res + "\n")
         }
 
+    }
+
+    /* AsyncTask <Parametros,Progreso,Resultado> */
+    inner class MiTarea: AsyncTask<Int, Void, Int>(){
+        override fun doInBackground(vararg params: Int?): Int {
+            var parametro:Int = params[0] as Int
+            return factorial(parametro)
+        }
+
+        override fun onPostExecute(result: Int?) {
+            salida.append(result.toString() + "\n")
+        }
     }
 }
